38be189ea5ee03fe7caa7ad3977de2dec53fe21d
Craig Treptow <craig.treptow@gmail.com>
2019-09-18 13:27:00 -0500

    task[lesson15]: Adds exercises and notes

---
+++ lesson15/one_time_pad.hs
@@ -0,0 +1,83 @@
+module OneTimePad where
+
+-- XOR truth table
+--
+-- First value  Second value  Result
+-- false        false         false
+-- true         false         true
+-- false        true          true
+-- true         true          false
+
+xorBool :: Bool -> Bool -> Bool
+xorBool value1 value2 = (value1 || value2) && (not (value1 && value2))
+
+xorPair :: (Bool,Bool) -> Bool
+xorPair (v1,v2) = xorBool v1 v2
+
+xor :: [Bool] -> [Bool] -> [Bool]
+xor list1 list2 = map xorPair (zip list1 list2)
+
+type Bits = [Bool]
+
+-- helper for final intToBits
+-- leaves result in reverse order
+-- GHCi> intToBits' 2
+-- [False,True]
+-- GHCi> intToBits' 8
+-- [False,False,False,True]
+
+intToBits' :: Int -> Bits
+intToBits' 0 = [False]
+intToBits' 1 = [True]
+intToBits' n = if (remainder == 0)
+               then False : intToBits' nextVal
+               else True : intToBits' nextVal
+  where remainder = n `mod` 2
+        nextVal = n `div` 2
+
+maxBits :: Int
+maxBits = length (intToBits' maxBound)
+
+-- make sure the result is the same size (in bits)
+-- of the e converted to bits
+intToBits :: Int -> Bits
+intToBits n = leadingFalses ++ reversedBits
+  where reversedBits = reverse (intToBits' n)
+        missingBits = maxBits - (length reversedBits)
+        leadingFalses = take missingBits (cycle [False])
+
+charToBits :: Char -> Bits
+charToBits char = intToBits (fromEnum char)
+
+bitsToInt :: Bits -> Int
+bitsToInt bits = sum (map (\x -> 2^(snd x)) trueLocations)
+  where size = length bits
+        indices = [size-1,size-2 .. 0]
+        trueLocations = filter (\x -> fst x == True)
+                        (zip bits indices)
+
+bitsToChar :: Bits -> Char
+bitsToChar bits = toEnum (bitsToInt bits)
+
+applyOTP' :: String -> String -> [Bits]
+applyOTP' pad plaintext = map (\pair -> (fst pair) `xor` (snd pair))
+                          (zip padBits plaintextBits)
+  where padBits = map charToBits pad
+        plaintextBits = map charToBits plaintext
+
+applyOTP :: String -> String -> String
+applyOTP pad plaintext = map bitsToChar bitList
+ where bitList = applyOTP' pad plaintext
+
+encoderDecoder :: String -> String
+encoderDecoder = applyOTP myPad
+
+-- for this to work (be suecure and unbreakable)
+-- the pad shoud be random.  This is not, so this is breakable
+-- because it will have patterns.
+myPad :: String
+myPad = "Shhhhhh"
+
+myPlainText :: String
+myPlainText = "Haskell"
+
