d18c25d575046099fec632e5130f95b12688f4f0
Craig Treptow <craig.treptow@gmail.com>
2019-09-27 19:36:06 -0500

    task[lesson19]: Adds exercises and notes

---
+++ lesson19/exercises.hs
@@ -0,0 +1,9 @@
+module Exercises where
+
+-- Q19.1
+-- Write emptyDrawers that takes the output of getDrawerContents
+-- and tells you the number of drawers that are empty
+
+-- See code in organs2.hs
+
+
