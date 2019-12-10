022ee26a7bbb6e351af1b08ffe8dbe59b97d674d
Craig Treptow <craig.treptow@gmail.com>
2019-12-10 13:57:33 -0600

    finished reading (done for now)

---
+++ lesson40/json-lesson/app/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+  
+import Data.Aeson
+import Data.Text as T
+import Data.ByteString.Lazy as B
+import Data.ByteString.Lazy.Char8 as BC
+import Lib
+
+main :: IO ()
+main = print "hi"
+
